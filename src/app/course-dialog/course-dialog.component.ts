5a975acf4aef2efdfced5f905a23c3c978d34666
Krzysiek <kw.winnicki@gmail.com>
2022-09-21 08:39:38 +0200

    Implementation of state management - optimistic save

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -3,11 +3,9 @@ import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
 import {Course} from '../model/course';
 import {FormBuilder, Validators, FormGroup} from '@angular/forms';
 import * as moment from 'moment';
-import {catchError} from 'rxjs/operators';
-import {throwError} from 'rxjs';
-import {CoursesService} from '../services/courses.service';
 import {LoadingService} from '../loading/loading.service';
 import {MessageServices} from '../messages/message.services';
+import {CoursesStore} from '../services/courses.store';
 
 @Component({
   selector: 'course-dialog',
@@ -20,25 +18,14 @@ import {MessageServices} from '../messages/message.services';
 })
 export class CourseDialogComponent implements AfterViewInit {
 
-  // zwrócmy uwagę na to, że LoadingService umieszczony jest jako osobny provider
-  // zarówno tu, jak i w app component
-  // stworzone zostaną zatem dwie instancje tego serwisu
-  // jednak, gdy wywołamy tutaj metodę włączającą loader (umieszczony w app.component) na widoku html
-  // to się on nie włączy - bo jest powiązany z instancją loader serwisu, ale tą z app. component
-  // i wszystkich umieszczonych poniżej w tej gałęzi komponentów
-  // okno dialogowe (material) pochodzi zaś z zupełnie innej gałęzi
-  // stąd na widoku html musimy dodać taki <loading>, aby loader zadziałał
-
   form: FormGroup;
-
   course: Course;
 
   constructor(
     private fb: FormBuilder,
     private dialogRef: MatDialogRef<CourseDialogComponent>,
     @Inject(MAT_DIALOG_DATA) course: Course,
-    private coursesService: CoursesService,
-    private loadingService: LoadingService,
+    private coursesStore: CoursesStore,
     private messagesService: MessageServices) {
 
     this.course = course;
@@ -55,30 +42,38 @@ export class CourseDialogComponent implements AfterViewInit {
   ngAfterViewInit() {
 
   }
-
   save() {
     const changes = this.form.value;
 
-    const saveCourses$ = this.coursesService.saveCourse(this.course.id, changes)
-      .pipe(
-        catchError(err => {
-          const message = 'Could not save course';
-          console.log(message, err);
-          this.messagesService.showErrors(message);
-          return throwError(err);
-        })
-      );
-
-    // i teraz, żeby zadziałał loader, zmieniamy zapis, aby metoda z loader serwisu
-    // obserwowała utwotrzoną wyżej zmienną
-
-    this.loadingService.showLoaderUntilCompleted(saveCourses$)
-      .subscribe(
-        val => {
-          this.dialogRef.close(val);
-        }
-      );
+    this.coursesStore.saveCourse(this.course.id, changes)
+      .subscribe();
+    this.dialogRef.close(changes);
   }
+
+
+  // save_OLD() {
+  //   const changes = this.form.value;
+  //
+  //   const saveCourses$ = this.coursesService.saveCourse(this.course.id, changes)
+  //     .pipe(
+  //       catchError(err => {
+  //         const message = 'Could not save course';
+  //         console.log(message, err);
+  //         this.messagesService.showErrors(message);
+  //         return throwError(err);
+  //       })
+  //     );
+  //
+  //   // i teraz, żeby zadziałał loader, zmieniamy zapis, aby metoda z loader serwisu
+  //   // obserwowała utwotrzoną wyżej zmienną
+  //
+  //   this.loadingService.showLoaderUntilCompleted(saveCourses$)
+  //     .subscribe(
+  //       val => {
+  //         this.dialogRef.close(val);
+  //       }
+  //     );
+  // }
 
   // WCZEŚNIEJ:
   // save() {
--- src/app/services/courses.store.ts
+++ src/app/services/courses.store.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {BehaviorSubject, Observable, throwError} from 'rxjs';
 import {Course, sortCoursesBySeqNo} from '../model/course';
-import {catchError, map, tap} from 'rxjs/operators';
+import {catchError, map, shareReplay, tap} from 'rxjs/operators';
 import {HttpClient} from '@angular/common/http';
 import {LoadingService} from '../loading/loading.service';
 import {MessageServices} from '../messages/message.services';
@@ -16,7 +16,7 @@ export class CoursesStore {
   // bez dostępu do wartości emitowanej przez subject (bo jest private)
   courses$: Observable<Course[]> = this.subject.asObservable();
 
-  constructor(private hhtp: HttpClient,
+  constructor(private http: HttpClient,
               private loading: LoadingService,
               private messages: MessageServices) {
     this.loadAllCourses();
@@ -32,8 +32,34 @@ export class CoursesStore {
       )
   }
 
+  saveCourse(courseId: string, changes: Partial<Course>): Observable<any> {
+    // last emitted value from subject
+    const courses = this.subject.getValue();
+
+    const index = courses.findIndex(course => course.id == courseId);
+
+    const newCourse: Course = {
+      ...courses[index],
+      ...changes
+    }
+    const newCourses: Course[] = courses.slice(0);
+    newCourses[index] = newCourse;
+
+    this.subject.next(newCourses);
+
+   return this.http.put(`/api/courses/${courseId}`, changes)
+     .pipe(
+       catchError(err => {
+         const message = 'Could not save courses';
+         this.messages.showErrors(message);
+         return throwError(err);
+       }),
+       shareReplay()
+     );
+  }
+
   private loadAllCourses() {
-    const loadCourses$ = this.hhtp.get<Course[]>('/api/courses')
+    const loadCourses$ = this.http.get<Course[]>('/api/courses')
       .pipe(
         map(response => response['payload']),
         catchError(err => {
